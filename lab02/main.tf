d056af62c6068f7a5cec74e934bd4e1d00a2297f
Amanda Jennings <38109573+agjenni80@users.noreply.github.com>
2018-10-18 13:54:20 -0400

    Update main.tf

--- lab02/main.tf
+++ lab02/main.tf
@@ -1,9 +1,12 @@
-module "server" {
+module "server-us-east-2" {
   source = "./server"
-  region       = "${var.region}"
+  region       = "us-east-2"
   num_webs     = "${var.num_webs}"
-  identity     = "${var.identity}"
-  ami          = "${lookup(var.ami, var.region)}"
+  identity     = "${var.identity}-us-east-2"
+  ami          = "${lookup(var.ami, "us-east-2")}"
   ingress_cidr = "${var.ingress_cidr}"
 }
 
+output "public_dns_us_east_2" {
+  value = "${module.server-us-east-2.public_dns}"
+}
